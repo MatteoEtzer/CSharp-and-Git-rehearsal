bea7b6e80a67148a1357de59c63c41544d7dabb8
MatteoEtzer <matteoetzer@gmx.at>
2020-11-24 17:53:26 +0100

    MasterScript

---
+++ Assets/Scripts/Master.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Master : MonoBehaviour
+{
+    public AudioSource audio;
+    public GameObject SoonGonnaBeSlave;
+    public Rigidbody rb;
+    public Light lamp;
+    public Camera cam;
+
+
+    void Start()
+    {
+        audio.mute = true;
+        SoonGonnaBeSlave.name = "Slave";
+        rb.drag = 15;
+        lamp.intensity = 30;
+        lamp.cookieSize = 300;
+        cam.depth = 123.45f;
+    }
+
+    void Update()
+    {
+    }
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,14 +4,35 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    public Transform Transformer;
+    public Transform tf;
     public MeshRenderer mr;
-    public CapsuleCollider KollidierenderKollidierer;
+    public CapsuleCollider cc;
     public Rigidbody rb;
+    public MeshFilter mf;
+    public Player play;
+    public GameObject cube;
+    public Material mat;
+    public Mesh m;
+    public int testvar;
+    private int x;
+
     
+
+
     void Start()
     {
-       
+       testvar = 15;
+       rb.useGravity = false;
+    }
+
+    public void ButtonClicked()
+    {
+        rb.mass = 15;
+        rb.useGravity = true;
+        tf.position = new Vector3(1f, 2f, 1f);
+        cc.radius = 4f;
+        mf.mesh = m;
+        play.testvar = 30;
     }
 
     void Update()
