f20e9f7191748c6eff302a743021f389d784a68b
MatteoEtzer <matteoetzer@gmx.at>
2020-11-24 16:43:08 +0100

    variable testing

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public Transform Transformer;
+    public MeshRenderer mr;
+    public CapsuleCollider KollidierenderKollidierer;
+    public Rigidbody rb;
+    
+    void Start()
+    {
+       
+    }
+
+    void Update()
+    {
+        
+    }
+}
